9610f91562b3eabdb3b50be7605e17564f0b9fd2
Matt Topper <matt@matttopper.com>
2010-11-12 21:55:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasswordVault::Application.config.secret_token = 'af91734bdaed135d207ce3b46249e5423ebfb272ee2f7e0462a7aaa1a717ea9a4475486ea7255140a7d0b68255a31569a5ce49039450c755335576f550f64448'
