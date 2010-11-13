2d93bdd0d1d70feb071116591fce1f99a40fa9b8
Matt Topper <topperge@msu.edu>
2010-11-12 23:35:52 -0500

    Start of Devise model and project subdomains

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 PasswordVault::Application.routes.draw do
+  get "users/index"
+
+  get "users/show"
+
+  devise_for :users
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ PasswordVault::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+puts 'SETTING UP EXAMPLE USERS'
+user1 = User.create! :first_name => 'First', :last_name => 'User', :username => 'firstuser', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user1.username
+user2 = User.create! :first_name => 'Second', :last_name => 'User', :username => 'seconduser', :email => 'otheruser@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user2.username
+#user1.save
+#user2.save
+puts 'SETTING UP EXAMPLE PROJECTS'
+project1 = Project.create! :project_name => 'First Project', :project_description => 'Project 1 Description', :nice_name => 'project1'
+puts 'New project created: ' << project1.project_name
+project2 = Project.create! :project_name => 'Second Project', :project_description => 'Project 2 Description', :nice_name => 'project2'
+puts 'New project created: ' << project2.project_name
+puts 'SETTING UP EXAMPLE SUBDOMAINS'
+subdomain1 = Subdomain.create! :name => 'project1'
+puts 'Created subdomain: ' << subdomain1.name
+subdomain2 = Subdomain.create! :name => 'project2'
+puts 'Created subdomain: ' << subdomain2.name
+project1.subdomains << subdomain1
+project1.save
+project2.subdomains << subdomain2
+project2.save
+
